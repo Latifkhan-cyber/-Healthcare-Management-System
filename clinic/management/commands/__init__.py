f7e7e6dff2d0754596a52acb5e0916d338e8637d
Latifkhan-cyber <latifkhan9232@gmail.com>
2026-06-13 17:27:10 +0500

    adding all the files to the staging area for commit

---
+++ clinic/management/commands/__init__.py
@@ -0,0 +1,8 @@
+"""
+Management commands for notification management.
+Place in: clinic/management/commands/
+
+Usage:
+    python manage.py send_appointment_reminders
+    python manage.py send_followup_reminders
+"""
